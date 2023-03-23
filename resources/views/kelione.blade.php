2b0362411917d9e6bcf03c2680cfc903c314fbe2
Lukas-Sciukas-IST20 <Lukas.Sciukas@stud.svako.lt>
2023-03-23 18:24:47 +0200

    kelioniu kurimas

--- resources/views/kelione.blade.php
+++ resources/views/kelione.blade.php
@@ -1,3 +1,11 @@
-<div>
-    testas<br>
-</div>
+<x-app-layout>
+    <x-slot name="header">
+    </x-slot>
+    <div>
+        @foreach($keliones as $kelione)
+        <div>
+            {{$kelione->aprasymas}}
+        </div>
+        @endforeach
+    </div>
+</x-app-layout>
